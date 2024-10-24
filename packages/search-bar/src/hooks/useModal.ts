ea0ca1c3323c5845bbd8f0620eb21cf14f48477d
ch1py7 <garcialopezgerardo6612@gmail.com>
2024-10-23 19:11:21 -0700

    feat: add value and setter to change state when the modal is closing

--- packages/search-bar/src/hooks/useModal.ts
+++ packages/search-bar/src/hooks/useModal.ts
@@ -1,14 +1,18 @@
 import { useEffect, useRef } from 'react'
 
-const clickListener = ({ target }: MouseEvent) => {
+const clickListener = (
+	{ target }: MouseEvent,
+	setOpen: React.Dispatch<React.SetStateAction<boolean>>
+) => {
 	const dialogTarget = target as HTMLElement
 
 	if (dialogTarget?.nodeName === 'DIALOG') {
 		;(dialogTarget as HTMLDialogElement)?.close('dismiss')
+    setOpen(false)
 	}
 }
 
-export const useModal = (open = false) => {
+export const useModal = (open: boolean, setOpen: React.Dispatch<React.SetStateAction<boolean>>) => {
 	const modalRef = useRef<HTMLDialogElement>(null)
 
 	useEffect(() => {
@@ -16,13 +20,13 @@ export const useModal = (open = false) => {
 		open ? modalRef.current.showModal() : modalRef.current.close()
 
 		open
-			? modalRef.current.addEventListener('click', clickListener)
-			: modalRef.current?.removeEventListener('click', clickListener)
+			? modalRef.current.addEventListener('click', (e) => clickListener(e, setOpen))
+			: modalRef.current?.removeEventListener('click', (e) => clickListener(e, setOpen))
 
 		return () => {
-			modalRef.current?.removeEventListener('click', clickListener)
+			modalRef.current?.removeEventListener('click', (e) => clickListener(e, setOpen))
 		}
-	}, [open])
+	}, [open, setOpen])
 
 	return { modalRef }
 }
